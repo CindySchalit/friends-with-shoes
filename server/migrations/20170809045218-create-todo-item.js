908779294b36c94f7bea87f26911fbcd4d9055f8
CindySchalit <cindyschalit@gmail.com>
2017-08-08 22:11:37 -0700

    Feat(App): configure migrations in progress

---
+++ server/migrations/20170809045218-create-todo-item.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('TodoItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      complete: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) =>
+    queryInterface.dropTable('TodoItems'),
+};

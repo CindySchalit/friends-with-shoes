7d158b41e038fda302a78573b2f9f75f253fe024
CindySchalit <cindyschalit@gmail.com>
2017-08-14 21:20:42 -0700

    Feat(front end): set up webpack

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from the front end!</h1>
+      </div>
+    )
+  }
+}

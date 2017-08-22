9a4dfa2709864830d8fda4f94341a74204d3c088
CindySchalit <cindyschalit@gmail.com>
2017-08-22 15:15:19 -0700

    Feat(App): refresh About and Shoes pages without redirecting to Home

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './Home.jsx'
 import About from './About.jsx'
@@ -12,6 +12,11 @@ const App = () => {
       <ul>
         <li>
           <nav>
+            <Link to="/">Home</Link>
+          </nav>
+        </li>
+        <li>
+          <nav>
             <Link to="/about">About</Link>
           </nav>
         </li>
@@ -21,10 +26,11 @@ const App = () => {
           </nav>
         </li>
       </ul>
-      <Redirect to="/home" push />
-      <Route path="/home" component={ Home } />
-      <Route path="/about" component={ About } />
-      <Route path="/shoes" component={ Shoes } />
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/about" component={ About } />
+        <Route path="/shoes" component={ Shoes } />
+      </Switch>
     </div>
   )
 }

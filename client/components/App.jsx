03daa4dd2fcd4804938f807e10e951caf7b2be30
CindySchalit <cindyschalit@gmail.com>
2017-08-22 18:33:50 -0700

    Chore(Routes): modularize routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import NavBar from './NavBar.jsx'
-import Home from './Home.jsx'
-import About from './About.jsx'
-import Shoes from './Shoes.jsx'
+import Routes from './Routes.jsx'
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <br />
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/shoes" component={ Shoes } />
-      </Switch>
+      <Routes />
     </div>
   )
 }

f8a670b4bc2f789dd5ccadd35af2dd9547eaa582
CindySchalit <cindyschalit@gmail.com>
2017-08-22 18:27:06 -0700

    Chore(NavBar): modularize NavBar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,34 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import NavBar from './NavBar.jsx'
 import Home from './Home.jsx'
 import About from './About.jsx'
 import Shoes from './Shoes.jsx'
 
-import styles from './styles/navBar.css'
-
 const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <nav>
-            <Link className="link" to="/">Home</Link>
-          </nav>
-        </li>
-        <li>
-          <nav>
-            <Link className="link" to="/about">About</Link>
-          </nav>
-        </li>
-        <li>
-          <nav>
-            <Link className="link" to="/shoes">Shoes</Link>
-          </nav>
-        </li>
-      </ul>
-      <br></br>
+      <NavBar />
+      <br />
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
---
+++ client/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './styles/navBar.css'
+
+export default class NavBar extends Component {
+  super(props) {
+
+  }
+
+  render() {
+    return(
+      <div>
+        <ul>
+          <li>
+            <nav>
+              <Link className="link" to="/">Home</Link>
+            </nav>
+          </li>
+          <li>
+            <nav>
+              <Link className="link" to="/about">About</Link>
+            </nav>
+          </li>
+          <li>
+            <nav>
+              <Link className="link" to="/shoes">Shoes</Link>
+            </nav>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}

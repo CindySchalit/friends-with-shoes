ade69a56afb83a411e35749f82e4f46876901ca7
CindySchalit <cindyschalit@gmail.com>
2017-08-25 09:32:59 -0700

    Feat(routes): refactor routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
 
 import NavBar from './NavBar.jsx'
-import Routes from '../../client/Routes.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Shoes from './Shoes.jsx'
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <br />
-      <Routes />
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/about" component={ About } />
+          <Route exact path="/shoes" component={ Shoes } />
+        </Switch>
+      </div>
     </div>
   )
 }

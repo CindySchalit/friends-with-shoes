53dfa3495b3dcd0118cbafba873d70f7a5de99ac
CindySchalit <cindyschalit@gmail.com>
2017-08-25 13:38:52 -0700

    Feat(App): remove react-router-redux

--- client/store.js
+++ client/store.js
@@ -1,13 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import createHistory from 'history/createHashHistory'
-import { routerMiddleware } from 'react-router-redux'
 
 import shoesReducer from './reducers/shoes'
 
-const store = createStore(
-  shoesReducer,
-  composeWithDevTools(applyMiddleware(routerMiddleware(history)))
-)
-
-export default store
+export default createStore(shoesReducer)

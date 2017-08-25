a702c3c178ad7f6246c51e50af51074d224dfe88
CindySchalit <cindyschalit@gmail.com>
2017-08-25 10:23:47 -0700

    Feat(App): create store

--- client/store.js
+++ client/store.js
@@ -1,1 +1,11 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createHistory from 'history/createHashHistory'
+import { routerMiddleware } from 'react-router-redux'
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(routerMiddleware(history)))
+)
+
+export default store

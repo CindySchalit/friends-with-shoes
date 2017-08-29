8c26e12fba907c8a95b148718768bae9b8f85ca1
CindySchalit <cindyschalit@gmail.com>
2017-08-28 22:38:16 -0700

    Feat(Shoes): make grid responsive

--- client/components/Shoes.jsx
+++ client/components/Shoes.jsx
@@ -7,7 +7,7 @@ const Shoes = (props) => {
   const shoes = props && props.shoes
 
   return(
-    <div>
+    <div className="Grid-Container-Shoes">
       <h1>Shoes Who Need Friends</h1>
       <br />
       <Grid fluid={true} className="Grid-Shoes">
@@ -15,10 +15,10 @@ const Shoes = (props) => {
           shoes.map((row, index) =>
             <Row className="show-grid" key={index}>{
               row.map(shoe =>
-                <Col className="Col-Shoes" key={shoe.id} xs={6} md={4}>
-                  <Image className="Image-Shoes" src={shoe.image} rounded/>
-                  <h3 className="header-Shoes">{shoe.shoeBrand}</h3>
-                  <h4 className="header-Shoes italics-Shoes">{shoe.shoeName}</h4>
+                <Col className="Col-Shoes" key={shoe.id} xs={6} md={4} lg={4}>
+                  <Image className="Image-Shoes" src={shoe.image} rounded responsive={true}/>
+                  <h4 className="header-Shoes">{shoe.shoeBrand}</h4>
+                  <h5 className="header-Shoes italics-Shoes">{shoe.shoeName}</h5>
                   Gender: {shoe.gender}
                   <br />
                   Size: {shoe.size}

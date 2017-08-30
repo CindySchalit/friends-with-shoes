ef417db26fa98a2b50ca7a31036057701e589254
CindySchalit <cindyschalit@gmail.com>
2017-08-30 11:43:06 -0700

    Feat(store): set up middleware

--- client/store.js
+++ client/store.js
@@ -1,5 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 
 import shoesReducer from './reducers/shoes'
 
-export default createStore(shoesReducer)
+export default createStore(
+  shoesReducer,
+  window._REDUX_DEVTOOLS_EXTENSION__ && window._REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(
+    thunkMiddleware,
+    createLogger({ collapsed: true })
+  )
+)

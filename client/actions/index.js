5a92e55a99a40717f650ca4c0af575751f7d9acf
CindySchalit <cindyschalit@gmail.com>
2017-08-30 11:10:23 -0700

    Feat(Shoes): add async action creator

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,10 +1,23 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8000/api'
+
 /* action types */
 export const GET_SHOES = 'GET_SHOES'
 
 /* action creators */
-export const getShoes = shoes => {
+const getShoes = shoes => {
   return {
     type: GET_SHOES,
     shoes
   }
 }
+
+const boundGetShoes = shoes => dispatch(getShoes(shoes))
+
+/* async action creators */
+export const getShoesThunk = () =>
+  dispatch =>
+    axios.get(`${API_URL}/shoes`)
+      .then(res => boundGetShoes(res.data))
+      .catch(error => console.log(error))

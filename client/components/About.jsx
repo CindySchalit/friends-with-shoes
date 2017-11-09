06a0c26da7eb23eed43f67af29f31c9ed78c6eba
CindySchalit <cindyschalit@gmail.com>
2017-11-09 11:13:08 -0800

    Feat(App): add masthead

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -5,7 +5,6 @@ import styles from './styles/about.css'
 const About = () => {
   return (
     <div className="aboutBody">
-      <h1 className="aboutHeader">Friends with Shoes</h1>
       <p className="aboutParagraph">Friends with Shoes is a marketplace for people who have feet of different sizes. For such persons, shoe shopping can be a hassle because they typically have to buy two pairs of the same shoe.</p>
       <p className="aboutParagraph">For instance, someone with a left foot of size 10 and a right foot of size 12 must buy two pairs of shoes - one of size 10 and another of size 12 - leaving him or her with an extra right shoe (size 12) and left shoe (size 10).</p>
       <p className="aboutParagraphLast">Here at Friends with Shoes, the reciprocally lopsided can exchange these extra shoes.</p>
--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import { HashRouter, Route, Link } from 'react-router-dom'
+
 import styles from './styles/navBar.css'
+import shoeRack from '../../public/shoeRack.png'
 
 const NavBar = () => {
   return(
-    <ul className="ul-NavBar">
-      <li className="li-NavBar">
-        <nav className="nav-NavBar">
-          <Link className="link-NavBar" to="/">Home</Link>
-        </nav>
-      </li>
-      <li className="li-NavBar">
-        <nav className="nav-NavBar">
-          <Link className="link-NavBar" to="/about">About</Link>
-        </nav>
-      </li>
-      <li className="li-NavBar">
-        <nav className="nav-NavBar">
-          <Link className="link-NavBar" to="/shoes">All the Shoes!</Link>
-        </nav>
-      </li>
-      <li className="li-NavBar">
-        <nav className="nav-NavBar">
-          <Link className="link-NavBar" to="/matches">Find a Shoe</Link>
-        </nav>
-      </li>
-    </ul>
+    <div>
+
+      <ul className="ul-NavBar">
+        <li className="li-NavBar">
+          <nav className="nav-NavBar">
+            <Link className="link-NavBar" to="/">Home</Link>
+          </nav>
+        </li>
+        <li className="li-NavBar">
+          <nav className="nav-NavBar">
+            <Link className="link-NavBar" to="/about">About</Link>
+          </nav>
+        </li>
+        <li className="li-NavBar">
+          <nav className="nav-NavBar">
+            <Link className="link-NavBar" to="/shoes">All the Shoes!</Link>
+          </nav>
+        </li>
+        <li className="li-NavBar">
+          <nav className="nav-NavBar">
+            <Link className="link-NavBar" to="/matches">Find a Shoe</Link>
+          </nav>
+        </li>
+      </ul>
+
+      <div className="Image-NavBar">
+        <h1>Friends with Shoes</h1>
+      </div>
+
+    </div>
   )
 }
 
